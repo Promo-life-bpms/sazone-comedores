e887859c7c8bddf1619368c510bb8a4f10782212
Antonio Tomas <antoniotd87@gmail.com>
2023-11-22 21:00:52 -0600

    Traduccion de laravel a español

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,11 +10,21 @@
             <label for="" class="text-lg font-semibold">Correo</label>
             <input type="email" name="email" placeholder="Ingrese su correo"
                 class="input input-bordered w-full @error('email') input-error @enderror" />
+            @error('email')
+                <div class="text-red-500">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="space-y-2">
             <label for="" class="text-lg font-semibold">Contraseña</label>
             <input type="password" name="password" placeholder="Ingrese su contraseña"
                 class="input input-bordered w-full @error('password') input-error @enderror" />
+            @error('email')
+                <div class="text-red-500">
+                    {{ $message }}
+                </div>
+            @enderror
             @if (Route::has('password.request'))
                 <div class="text-right h-16">
                     <a class="" href="{{ route('password.request') }}">
