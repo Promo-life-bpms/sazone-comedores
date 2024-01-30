cf69203b8d63467499fb914ae4f1652198209f54
Oscar-CR <krz.oficial@gmail.com>
2024-01-30 13:19:40 -0600

    fix ruta de logout

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 Auth::routes();
 Route::get('/loginEmail', [LoginController::class, 'loginWithLink'])->name('loginWithLink');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/logoutUser', [LoginController::class, 'logoutUser'])->name('logoutUser');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
