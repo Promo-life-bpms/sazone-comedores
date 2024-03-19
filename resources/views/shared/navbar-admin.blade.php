4b95b9b625339e5e5a5d0f1c1e322d6838cb687a
Oscar-CR <krz.oficial@gmail.com>
2024-03-18 23:46:21 -0600

    agregado de nueva funcionalidad para asignar admins

--- resources/views/shared/navbar-admin.blade.php
+++ resources/views/shared/navbar-admin.blade.php
@@ -4,8 +4,19 @@
     </div>
     <div class="flex-none">
         <ul class="menu menu-horizontal px-1 py-0 gap-x-3">
+            @if (Auth::user()->hasRole(['master-admin']))
             <li>
-                <a class="flex flex-col p-0" href="{{ route('mi-cuenta') }}">
+                <a class="flex flex-col p-0 pr-2" href="{{ route('dining.admins') }}">
+                    <img alt="logo" class="rounded-full border w-10 h-10 object-cover"
+                        src="{{ asset('assets/users.png') }}" />
+                    <p class="text-xs font-semibold">
+                        Administradores
+                    </p>
+                </a>
+            </li>
+            @endif
+            <li>
+                <a class="flex flex-col p-0 pl-4" href="{{ route('mi-cuenta') }}">
                     <img alt="logo" class="rounded-full border w-10 h-10 object-cover"
                         src="{{ asset('assets/user.png') }}" />
                     <p class="text-xs font-semibold">
@@ -13,6 +24,7 @@
                     </p>
                 </a>
             </li>
+            
         </ul>
     </div>
 </div>
