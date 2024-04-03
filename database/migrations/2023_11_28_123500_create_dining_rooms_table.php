d3c6d8342893d2984bfb3fd46635600b6a05e8a3
Oscar-CR <krz.oficial@gmail.com>
2024-04-03 15:58:31 -0600

    agregado de nuevo atributo de usuario y comedor en migraciones

--- database/migrations/2023_11_28_123500_create_dining_rooms_table.php
+++ database/migrations/2023_11_28_123500_create_dining_rooms_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->text('values')->nullable();
             $table->string('slug');
             $table->text('customization')->nullable();
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
