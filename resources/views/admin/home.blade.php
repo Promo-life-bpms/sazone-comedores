2a5c215a8648873fe927274a44f9aee93a4e52f0
Oscar-CR <krz.oficial@gmail.com>
2024-05-30 15:38:00 -0600

    delimitacion de roles en vista administrador

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -20,52 +20,7 @@
 
             <p class="text-2xl font-bold">{{ $diningRoom->name }}</p>
             <p class="text-lg pb-5">{{ $diningRoom->address }}</p>
-
-            {{-- <div class="collapse collapse-plus bg-base-200">
-            <input type="radio" name="my-accordion-3" />
-            <div class="collapse-title text-xl font-medium">
-                Detalles Generales
-            </div>
-            <div class="collapse-content">
-                @php
-                    $customization = (object) json_decode($diningRoom->customization);
-                @endphp
-                <form action="{{ route('dining.updateDetailsGeneral', ['dining' => $diningRoom]) }}" method="POST"
-                    enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
-                    <div class="grid grid-cols-4 gap-x-3">
-                        <div class="col-span-1">
-                            <label for="">Logotipo</label>
-                            <input type="file" name="logo" class="file-input file-input-bordered w-full">
-                        </div>
-                        <div class="col-span-1">
-                            <label for="">Color Primario</label>
-                            <input type="color" name="primary" placeholder="Type here" class="input input-bordered w-full"
-                                value="{{ $customization->primary_color ?? '' }}" />
-                        </div>
-                        <div class="col-span-1">
-                            <label for="">Color Secundario</label>
-                            <input type="color" name="secondary" placeholder="Type here"
-                                class="input input-bordered w-full" value="{{ $customization->secondary_color ?? '' }}" />
-                        </div>
-                        <div class="col-span-1 flex justify-center items-end">
-                            <button class="btn btn-primary w-full">Actualizar</button>
-                        </div>
-                    </div>
-                </form>
-                @if (session('success'))
-                    <div role="alert" class="alert alert-success">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none"
-                            viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                        <span>{{ session('success') }}</span>
-                    </div>
-                @endif
-            </div>
-            </div> --}}
+            
             <div class="collapse collapse-plus bg-stone-100 " >
                 <input type="radio" name="my-accordion-3"
                     @if (session('section')) {{ session('section') == 'advertisements' ? 'checked' : '' }} @endif />
@@ -77,18 +32,7 @@
                     @include('admin.sections.advertisment.index')
                 </div>
             </div>
-            <div class="mt-6"></div>
-            {{-- <div class="collapse collapse-plus bg-stone-100">
-            <input type="radio" name="my-accordion-3"
-            @if (session('section')) {{ session('section') == 'cupones' ? 'checked' : '' }} @endif
-            />
-            <div class="collapse-title text-xl font-medium">
-                Cupones
-            </div>
-            <div class="collapse-content">
-                @include('admin.sections.cupon.index')
-            </div>
-         </div> --}}
+           
             <div class="mt-6">
                 <div class="collapse collapse-plus bg-stone-100">
                     <input type="radio" name="my-accordion-3"
@@ -101,6 +45,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="mt-6">
                 <div class="collapse collapse-plus bg-stone-100">
                     <input type="radio" name="my-accordion-3" 
@@ -115,20 +60,7 @@
                     </div>
                 </div>
             </div>
-          {{--   <div class="mt-6">
-                <div class="collapse collapse-plus bg-stone-100  ">
-                    <input type="radio" name="my-accordion-3"
-                    @if (session('section'))
-                        {{ session('section') == 'capsulas' ? 'checked' : '' }}
-                    @endif/>
-                    <div class="collapse-title text-xl font-medium">
-                        Capsulas de Nutricion
-                    </div>
-                    <div class="collapse-content overflow-x-auto">
-                        @include('admin.sections.capsula.index')
-                    </div>
-                </div>
-            </div> --}}
+       
             <div class="mt-6">
                 <div class="collapse collapse-plus bg-stone-100  ">
                     <input type="radio" name="my-accordion-3"
@@ -143,6 +75,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="mt-6">
                 <div class="collapse collapse-plus bg-stone-100  ">
                     <input type="radio" name="my-accordion-3"
@@ -174,6 +107,7 @@
                     </div>
                 </div>
             </div>
+
         @endif
     </div>
 @endsection
